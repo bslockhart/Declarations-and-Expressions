a569c9e67c09cdd3ced346ff3fb6f8ac4fd1eba4
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-18 20:30:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+* Functions are blocks of code that contain instructions to be executed as needed.
+* Functions can contain commands, variables, and additional calls within itself.
+* Two types of functions: 1. Function Declaration 2. Function Expression# Declarations-and-Expressions
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Function Declaration and Expression
+
+// This is myDeclaration
+function myDeclaration() {
+    alert("This comes from a declaration.");
+};
+
+// Call the function to execute the code
+myDeclaration();
+
+// This is myExpression
+var myExpression = function() {
+    alert("This comes from an expression")
+}
+
+myExpression();
